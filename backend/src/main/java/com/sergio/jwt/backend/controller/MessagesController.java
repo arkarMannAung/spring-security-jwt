2269840dbad6e69302215715ef210c3bce178985
arkarMannAung <mr.arkarmannaung@gmail.com>
2023-07-12 16:43:55 +0630

    first

---
+++ backend/src/main/java/com/sergio/jwt/backend/controller/MessagesController.java
@@ -0,0 +1,17 @@
+package com.sergio.jwt.backend.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessagesController {
+
+	@GetMapping("/messages")
+	public ResponseEntity<List<String>> message(){
+		return ResponseEntity.ok(Arrays.asList("first","second","third","four"));
+	}
+}
